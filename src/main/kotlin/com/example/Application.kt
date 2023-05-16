93b50347f564ad6c45dfd3837dd2795e41ae11f6
Nikita <androidChill@yandex.ru>
2023-05-16 22:51:04 +0400

    fix module application

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -20,59 +20,8 @@ import org.koin.ktor.ext.get
 import org.koin.ktor.plugin.Koin
 
 fun main() {
-    embeddedServer(Netty, port = (System.getenv("PORT") ?: "8080").toInt()) {
-        module(this)
-    }
-        .start(wait = true)
-}
-
-fun module(application: Application) {
-
-    with(application) {
-        install(Koin) {
-            modules(databaseModule)
-            modules(userModule)
-            modules(questioningModule)
-            modules(friendsModule)
-        }
-
-        install(Authentication) {
-            jwt {
-                verifier(JwtConfig.verifier)
-                realm = "com.example.rom_dv"
-                validate {
-                    val id = it.payload.getClaim("id").asInt()
-                    val nickname = it.payload.getClaim("nickname").asString()
-                    val password = it.payload.getClaim("password").asString()
-                    if (nickname != null && password != null) {
-                        UserPrincipal(id, nickname, password)
-                    } else {
-                        null
-                    }
-                }
-            }
-        }
-
-        routing {
-            val userController: UserController = get()
-            val questioningController: QuestioningController = get()
-            val friendsController: FriendsController = get()
-
-            userController.userRoute(this)
-            questioningController.questioningRoute(this)
-            friendsController.friendsRoute(this)
-        }
-
-        configureHTTP()
-        configureMonitoring()
-        configureSerialization()
-        configureDatabases()
-        configureSockets()
-        configureAdministration()
-        configureRouting()
-    }
-
-
+    embeddedServer(Netty, port = (System.getenv("PORT") ?: "8080").toInt(),  module = Application::module)
+            .start(wait = true)
 }
 
 val ApplicationCall.user get() = authentication.principal<UserPrincipal>()
---
+++ src/main/kotlin/com/example/Module.kt
@@ -0,0 +1,62 @@
+package com.example
+
+import com.example.core.database.databaseModule
+import com.example.feature.friend.friendsModule
+import com.example.feature.friend.presentation.FriendsController
+import com.example.feature.questioning.presentation.QuestioningController
+import com.example.feature.questioning.questioningModule
+import com.example.feature.user.presentation.UserController
+import com.example.feature.user.userModule
+import com.example.plugins.*
+import com.example.utils.JwtConfig
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.get
+import org.koin.ktor.plugin.Koin
+
+fun Application.module() {
+
+    install(Koin) {
+        modules(databaseModule)
+        modules(userModule)
+        modules(questioningModule)
+        modules(friendsModule)
+    }
+
+    install(Authentication) {
+        jwt {
+            verifier(JwtConfig.verifier)
+            realm = "com.example.rom_dv"
+            validate {
+                val id = it.payload.getClaim("id").asInt()
+                val nickname = it.payload.getClaim("nickname").asString()
+                val password = it.payload.getClaim("password").asString()
+                if (nickname != null && password != null) {
+                    UserPrincipal(id, nickname, password)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+    routing {
+        val userController: UserController = get()
+        val questioningController: QuestioningController = get()
+        val friendsController: FriendsController = get()
+
+        userController.userRoute(this)
+        questioningController.questioningRoute(this)
+        friendsController.friendsRoute(this)
+    }
+
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureDatabases()
+    configureSockets()
+    configureAdministration()
+    configureRouting()
+}
