52d19a18f3a6e34209121fbe70146068db1d2efa
Nikita <androidChill@yandex.ru>
2023-05-17 01:25:54 +0400

    fix module application

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -21,50 +21,53 @@ import org.koin.ktor.plugin.Koin
 
 fun main() {
     val port = System.getenv("PORT")?.toInt() ?: 23567
-    embeddedServer(Netty, port = port) {
-        install(Koin) {
-            modules(databaseModule)
-            modules(userModule)
-            modules(questioningModule)
-            modules(friendsModule)
-        }
+    embeddedServer(Netty, port = port, module = Application::module)
+            .start(wait = true)
+}
 
-        install(Authentication) {
-            jwt {
-                verifier(JwtConfig.verifier)
-                realm = "com.example"
-                validate {
-                    val id = it.payload.getClaim("id").asInt()
-                    val nickname = it.payload.getClaim("nickname").asString()
-                    val password = it.payload.getClaim("password").asString()
-                    if (nickname != null && password != null) {
-                        UserPrincipal(id, nickname, password)
-                    } else {
-                        null
-                    }
+@Suppress("unused")
+fun Application.module() {
+    install(Koin) {
+        modules(databaseModule)
+        modules(userModule)
+        modules(questioningModule)
+        modules(friendsModule)
+    }
+
+    install(Authentication) {
+        jwt {
+            verifier(JwtConfig.verifier)
+            realm = "com.example"
+            validate {
+                val id = it.payload.getClaim("id").asInt()
+                val nickname = it.payload.getClaim("nickname").asString()
+                val password = it.payload.getClaim("password").asString()
+                if (nickname != null && password != null) {
+                    UserPrincipal(id, nickname, password)
+                } else {
+                    null
                 }
             }
         }
-
-        routing {
-            val userController: UserController = get()
-            val questioningController: QuestioningController = get()
-            val friendsController: FriendsController = get()
-
-            userController.userRoute(this)
-            questioningController.questioningRoute(this)
-            friendsController.friendsRoute(this)
-        }
-
-        configureHTTP()
-        configureMonitoring()
-        configureSerialization()
-        configureDatabases()
-        configureSockets()
-        configureAdministration()
-        configureRouting()
     }
-            .start(wait = true)
+
+    routing {
+        val userController: UserController = get()
+        val questioningController: QuestioningController = get()
+        val friendsController: FriendsController = get()
+
+        userController.userRoute(this)
+        questioningController.questioningRoute(this)
+        friendsController.friendsRoute(this)
+    }
+
+    configureHTTP()
+    configureMonitoring()
+    configureSerialization()
+    configureDatabases()
+    configureSockets()
+    configureAdministration()
+    configureRouting()
 }
 
 val ApplicationCall.user get() = authentication.principal<UserPrincipal>()
--- src/main/kotlin/com/example/Module/Module.kt
+++ src/main/kotlin/com/example/Module/Module.kt
@@ -18,45 +18,45 @@ import org.koin.ktor.plugin.Koin
 
 fun Application.module() {
 
-    install(Koin) {
-        modules(databaseModule)
-        modules(userModule)
-        modules(questioningModule)
-        modules(friendsModule)
-    }
-
-    install(Authentication) {
-        jwt {
-            verifier(JwtConfig.verifier)
-            realm = "com.example"
-            validate {
-                val id = it.payload.getClaim("id").asInt()
-                val nickname = it.payload.getClaim("nickname").asString()
-                val password = it.payload.getClaim("password").asString()
-                if (nickname != null && password != null) {
-                    UserPrincipal(id, nickname, password)
-                } else {
-                    null
-                }
-            }
-        }
-    }
-
-    routing {
-        val userController: UserController = get()
-        val questioningController: QuestioningController = get()
-        val friendsController: FriendsController = get()
-
-        userController.userRoute(this)
-        questioningController.questioningRoute(this)
-        friendsController.friendsRoute(this)
-    }
-
-    configureHTTP()
-    configureMonitoring()
-    configureSerialization()
-    configureDatabases()
-    configureSockets()
-    configureAdministration()
-    configureRouting()
+//    install(Koin) {
+//        modules(databaseModule)
+//        modules(userModule)
+//        modules(questioningModule)
+//        modules(friendsModule)
+//    }
+//
+//    install(Authentication) {
+//        jwt {
+//            verifier(JwtConfig.verifier)
+//            realm = "com.example"
+//            validate {
+//                val id = it.payload.getClaim("id").asInt()
+//                val nickname = it.payload.getClaim("nickname").asString()
+//                val password = it.payload.getClaim("password").asString()
+//                if (nickname != null && password != null) {
+//                    UserPrincipal(id, nickname, password)
+//                } else {
+//                    null
+//                }
+//            }
+//        }
+//    }
+//
+//    routing {
+//        val userController: UserController = get()
+//        val questioningController: QuestioningController = get()
+//        val friendsController: FriendsController = get()
+//
+//        userController.userRoute(this)
+//        questioningController.questioningRoute(this)
+//        friendsController.friendsRoute(this)
+//    }
+//
+//    configureHTTP()
+//    configureMonitoring()
+//    configureSerialization()
+//    configureDatabases()
+//    configureSockets()
+//    configureAdministration()
+//    configureRouting()
 }
